2ec8a7c0d6db80f354149c8e1273d26942feff4e
mpross512 <mpross512@gmail.com>
2018-12-28 16:34:45 -0600

    Started Day Three

---
+++ Day3/DayThree.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.IOException;
+
+public class DayThree {
+
+    public static void main(String[]args) throws IOException {
+        File file = new File("input.txt");
+        System.out.println("Puzzle A: " + getChecksumA(file));
+    }
+
+    public static int getChecksumA(File file) throws IOException {
+        return 0;
+    }
+
+}
